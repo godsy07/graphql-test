c923285a8c50214806e32b4ed4c4c1c1a844ff46
unknown <godsonemmanuel07@gmail.com>
2021-09-01 17:45:42 +0530

    Setup for feytching data from pokemon api

---
+++ src/components/pokemon-container/PokemonContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./pokemon-container.styles.css";
+
+const PokemonContainer = () => {
+  return (
+    <div className='bg-gray-300 m-8 p-2 w-full rounded-xl justify-center text-center'>
+      <h1 className=''>Pokemon List</h1>
+    </div>
+  );
+};
+
+export default PokemonContainer;
