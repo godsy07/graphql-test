f8b7187e9bc9ba853b832573eb283d059fb89963
unknown <godsonemmanuel07@gmail.com>
2021-09-01 18:34:50 +0530

    setup apollo client'

--- src/components/pokemon-container/PokemonContainer.jsx
+++ src/components/pokemon-container/PokemonContainer.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
 import "./pokemon-container.styles.css";
 
+const GET_DOGS = gql`
+  query samplePokeAPIquery {
+    gen3_species: pokemon_v2_pokemonspecies(where: {}) {
+      name
+      id
+    }
+  }
+`;
+
 const PokemonContainer = () => {
+  const { loading, error, data } = useQuery(GET_DOGS);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  console.log(data);
   return (
     <div className='bg-gray-300 m-8 p-2 w-full rounded-xl justify-center text-center'>
       <h1 className=''>Pokemon List</h1>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://beta.pokeapi.co/graphql/v1beta",
+  cache: new InMemoryCache(),
+});
+
+// client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
